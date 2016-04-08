a2bcddff55e05b2ff0abd8ae969df7de49c23de4
cocoflan <christophe.kinet@telenet.be>
2016-04-08 13:17:08 +0200

    Initial commit

---
+++ random password.py
@@ -0,0 +1,17 @@
+
+
+
+import string
+import random
+print ('Random Password')
+print ('--------------')
+print
+def randompassword():
+    chars=string.ascii_uppercase + string.ascii_lowercase + string.digits
+    size= 0
+
+    return ''.join(random.choice(chars) for x in range(size,60))
+
+print(randompassword())
+
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Random password generator',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='COCOFLAN',
+    author_email='',
+    description=''
+)
