36bbff90b43814f4573a961585735931a6366c86
cocoflan <christophe.kinet@telenet.be>
2016-04-11 09:57:09 +0200

    test

--- random password.py
+++ random password.py
@@ -6,22 +6,22 @@ import random
 print ('Random Password')
 print ('---------------')
 print
+x = input('lengte:')
 def randompassword():
     chars=string.ascii_uppercase + string.ascii_lowercase + string.digits
     size= 0
 
-    return ''.join(random.choice(chars) for x in range(size,30))
+    return ''.join(random.choice(chars) for x in range(size, +x))
 
 print(randompassword())
 print
-print ('30 characters')
 print
+y = input('lengte:')
 def randompassword2():
     chars=string.ascii_uppercase + string.ascii_lowercase + string.digits
     size= 0
 
-    return ''.join(random.choice(chars) for x in range(size,60))
+    return ''.join(random.choice(chars) for x in range(size, +y))
 
 print(randompassword2())
 print
-print ('60 characters')
