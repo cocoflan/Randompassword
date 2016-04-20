538c9d2ad19f78ce6df56e6d90767d4cac13e1fe
cocoflan <christophe.kinet@telenet.be>
2016-04-20 10:55:18 +0200

    test

--- random password.py
+++ random password.py
@@ -3,7 +3,7 @@
 
 import string
 import random
-print ('Random Password')
+print ('5 Random Password')
 print ('---------------')
 print
 x = input('lengte:')
@@ -14,14 +14,7 @@ def randompassword():
     return ''.join(random.choice(chars) for x in range(size, +x))
 
 print(randompassword())
-print
-print
-y = input('lengte:')
-def randompassword2():
-    chars=string.ascii_uppercase + string.ascii_lowercase + string.digits
-    size= 0
-
-    return ''.join(random.choice(chars) for x in range(size, +y))
-
-print(randompassword2())
-print
+print(randompassword())
+print(randompassword())
+print(randompassword())
+print(randompassword())
