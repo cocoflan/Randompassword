1684b4251c644d9b34f9dcf8eb252f2a4815005e
cocoflan <christophe.kinet@telenet.be>
2016-04-23 10:19:15 +0200

    test

--- random password.py
+++ random password.py
@@ -5,7 +5,7 @@
 import string
 import random
 print ('5 Random Password')
-print ('---------------')
+print ('-----------------')
 print
 x = input('lengte:')
 def randompassword():
@@ -19,3 +19,8 @@ print(randompassword())
 print(randompassword())
 print(randompassword())
 print(randompassword())
+
+print
+
+raw_input("Press Enter to Exit")
+
